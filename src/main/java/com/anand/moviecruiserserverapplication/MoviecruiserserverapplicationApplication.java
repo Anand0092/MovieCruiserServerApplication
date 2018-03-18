a439256d418ed078bb64d4819d1b65280b425709
anand0092 <a.anand151192@gmail.com>
2018-03-18 16:19:05 +0530

    Initial commit

---
+++ src/main/java/com/anand/moviecruiserserverapplication/MoviecruiserserverapplicationApplication.java
@@ -0,0 +1,12 @@
+package com.anand.moviecruiserserverapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviecruiserserverapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviecruiserserverapplicationApplication.class, args);
+	}
+}
